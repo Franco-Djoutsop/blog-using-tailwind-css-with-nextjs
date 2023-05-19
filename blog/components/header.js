7a485ecb0c28b1d89dd3d0f2643c8ed112d01ab9
Franck Brice <francodjoutsop@gmail.com>
2023-05-19 11:59:15 +0200

    fixed header.js

--- blog/components/header.js
+++ blog/components/header.js
@@ -5,7 +5,7 @@ export default function header(){
     return(
         <header className="bg-blue-50">
             <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-                <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-cnter py-4 sm:py-0">
+                <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
                     <input className="input-text" type="text" placeholder="search" />
                 </div>
                 <div className="shrink w-80 sm:order-2">
